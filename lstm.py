344dc667c04c2bef186514fe680c76b9086a2038
Subarno <subarnopal.babai@gmail.com>
2017-10-10 22:15:55 +0530

    final update

--- lstm.py
+++ lstm.py
@@ -61,8 +61,8 @@ trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
 print('Train Score: %.2f RMSE' % (trainScore))
 testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
 print('Test Score: %.2f RMSE' % (testScore))
-# shift train predictions for plotting
 
+# shift train predictions for plotting
 trainPredictPlot = numpy.empty_like(dataset)
 trainPredictPlot[:, :] = numpy.nan
 trainPredictPlot[look_back:len(trainPredict)+look_back, :] = trainPredict
